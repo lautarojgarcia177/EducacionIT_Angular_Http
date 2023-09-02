350a3955a6f68e4bae204edd0975895aa24aeece
Lautaro Garcia <lautaro.garcia@intive.com>
2023-09-01 22:18:42 -0300

    subo todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PokemonService } from './pokemon.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'SuperLista__Angular';
+  resultados: any = [];
+  constructor(pokemonService: PokemonService) {
+    pokemonService
+      .buscarPersonajeYDevolverImagenes('Rick')
+      // .pipe(
+      //   /* Forma 2 */
+      //   map((respuesta) => respuesta.results.map((result: any) => result.image))
+      // )
+      .subscribe({
+        next: (respuesta) => {
+          /* Forma 1: */
+          // this.resultados = personaje.results.map((r: any) => r.image)
+          this.resultados = respuesta;
+          console.log(respuesta);
+        },
+      });
+  }
 }
